269363476090a3c10344ea8a654fb70ed53133c2
Győző Horváth <gyozo.horvath@inf.elte.hu>
2020-04-17 10:02:22 +0200

    7. gyakorlat

--- src/index.js
+++ src/index.js
@@ -4,10 +4,28 @@ import ReactDOM from 'react-dom';
 import 'fomantic-ui-css/semantic.min.css';
 
 import { App } from "./views/App";
+import { PlaylistsProvider } from './state/PlaylistsProvider';
+import { playlistsStorage } from './api/PlaylistsStorage';
+import { examplePlaylists } from './domain/playlist';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const render = () =>
+  ReactDOM.render(
+    <PlaylistsProvider>
+      <App />
+    </PlaylistsProvider>,
+    document.getElementById('root')
+  );
+
+async function start() {
+  const newPlaylists = await playlistsStorage.fill(examplePlaylists)
+  // console.log(newPlaylists);
+
+  // const newPlaylist = await playlistsStorage.create({ title: 'Something', tracks: [] })
+  // console.log(newPlaylist);
+
+  // const playlists = await playlistsStorage.getAll()
+  // console.log(playlists);
+
+  render()
+}
+start()
---
+++ src/state/PlaylistsProvider.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { playlistsStorage } from "../api/PlaylistsStorage";
+
+const usePlaylistsService = () => {
+  // Data
+  const [playlists, setPlaylists] = useState([])
+
+  useEffect(() => {
+    const getAll = async () => setPlaylists(await playlistsStorage.getAll())
+    getAll()
+  }, [])
+  // console.log(playlists);
+
+  // Operations
+  const addNewPlaylist = async title => {
+    const newPlaylist = await playlistsStorage.create({ title, tracks: [] })
+    setPlaylists([...playlists, newPlaylist])
+  }
+
+  // Service
+  const playlistsService = { playlists, addNewPlaylist }
+
+  return playlistsService
+}
+
+export const PlaylistsContext = React.createContext()
+export const PlaylistsProvider = ({ children }) => {
+  const playlistsService = usePlaylistsService()
+  return <PlaylistsContext.Provider value={playlistsService}>{children}</PlaylistsContext.Provider>
+}
