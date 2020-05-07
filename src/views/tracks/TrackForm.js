37b6570fb7ec64e92a13be468022fa295d98005b
Győző Horváth <gyozo.horvath@inf.elte.hu>
2020-05-07 12:23:12 +0200

    Resolution for the error in TrackForm when pressing ENTER

--- src/views/tracks/TrackForm.js
+++ src/views/tracks/TrackForm.js
@@ -61,7 +61,7 @@ export function TrackForm({ open, onClose, onSubmit, track }) {
         </div>
       </div>
       <div className="actions">
-        <button onClick={onClose} className="ui black deny button">
+        <button type="button" onClick={onClose} className="ui black deny button">
           Cancel
         </button>
         <button type="submit" className="ui positive right labeled icon button">
